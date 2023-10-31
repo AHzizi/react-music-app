4ee62b416a6b2f51f15478500b29248fea3657fc
AHzizi <miftakulazizi0@gmail.com>
2023-10-31 08:27:42 +0700

    fix title

--- src/Elements/Library/LibrarySongTitle.js
+++ src/Elements/Library/LibrarySongTitle.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 function LibrarySongTitle({ song }) {
-    return (
-        <h4 className="library__song--title">{song.title}
-        </h4>
-    )
+     return <h4 className="library__song--title">{song.title}</h4>;
+        
+
 }
 
 export default LibrarySongTitle;
